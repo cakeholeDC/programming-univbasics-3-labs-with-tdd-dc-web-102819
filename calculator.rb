617b0581032c0e8fcc43d192eb126a32082c8d57
Kyle Cole <kylepcole@gmail.com>
2019-10-03 23:09:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,8 @@
 # Add your variables here
-first_number = 7;
-second_number = 8;
+first_number = 7
+second_number = 8
 
-sum = first_number + second_number;
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number * second_number
